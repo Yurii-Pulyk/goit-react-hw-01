87d37e3ec4b05c248c58ba64a904a0b2a7cb8e6c
Yurii_Pulyk <Lemberger750@gmail.com>
2025-02-22 15:32:08 +0200

    hw1/task3

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,38 +1,23 @@
-import PropTypes from 'prop-types';
-import styles from './TransactionHistory.module.css';
-
-const TransactionHistory = ({ items }) => {
+export default function TransactionHistory({ items }) {
   return (
-    <table className={styles.transactionHistory}>
+    <table>
       <thead>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th>Type </th>
+          <th>Amount </th>
+          <th>Currency </th>
         </tr>
       </thead>
+
       <tbody>
-        {items.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}</td>
+            <td>{item.currency}</td>
           </tr>
         ))}
       </tbody>
     </table>
   );
-};
-
-TransactionHistory.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-      amount: PropTypes.string.isRequired,
-      currency: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-};
-
-export default TransactionHistory;
+}
